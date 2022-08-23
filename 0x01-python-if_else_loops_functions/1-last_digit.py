99e0721d0fc8edbe5761cda7c4f31b433af688a4
Isghak <isghakcollins9@gmail.com>
2022-08-23 11:45:28 -0700

    Hot Fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
-num_str = repr (number)
-last_digit_str = num_str[-1]
-last_digit = int(last_digit_str)
-
 if number < 0:
-    last_digit = number % -10
+    lastd = number % -10
 else:
-    last_digit = number % 10
-
-if last_digit > 5 :
-    print(f"Last digit of {number} is {last_digit} and is greater then 5")
-
-elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
-
-elif 6 > last_digit and last_digit != 0:
-    print(f"Last digit of {number} is {last_digit} and is less then 6 and not 0")
+    lastd = number % 10
+print('Last digit of {} is '.format(number), end="")
+if lastd > 5:
+    print('{} and is greater than 5'.format(lastd))
+elif lastd == 0:
+    print('{} and is 0'.format(lastd))
+elif lastd < 6 and lastd != 0:
+    print('{} and is less than 6 and not 0'.format(lastd))
---
+++ 0x01-python-if_else_loops_functions/1-test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+num_str = repr (number)
+last_digit_str = num_str[-1]
+last_digit = int (last_digit_str)
+
+if number < 0:
+    last_digit = number % -10
+else:
+    last_digit = number % 10
+
+if last_digit > 5:
+    print(f"Last digit of {number} is {last_digit} and is greater then 5")
+
+elif last_digit == 0:
+    print(f"Last digit of {number} is {last_digit} and is 0")
+
+elif 6 > last_digit and last_digit != 0:
+    print(f"Last digit of {number} is {last_digit} and is less then 6 and not 0")
